3c439591786ae27082c8b2f67bd195c97c3ab4b6
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-10 12:22:30 +0600

    doing a js project

---
+++ js/bank.js
@@ -0,0 +1,16 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+    // get user email
+    const emailFild = document.getElementById('user-email');
+    const userEmail = emailFild.value;
+    
+    // get user password
+    const passwordlFild = document.getElementById('user-password');
+    const userPassword = passwordlFild.value;
+    // check user email & password
+    if(userEmail == 'shuvo@gmail.com' && userPassword == 'shuvo' ){
+        window.location.href = "banking.html";
+    }else{
+        console.log(alert('c'))
+    }
+    
+})
